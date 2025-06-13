3c21bfce7d81e9326c619608a0c5353816c0e7ec
Azizbek Hafizov <azizbekhafizov678@gmail.com>
2025-06-13 16:55:02 +0500

    new

--- src/data/teachers.js
+++ src/data/teachers.js
@@ -4,6 +4,8 @@ const teachers = [
   { login: "marjona", password: "885764010", subject: "Ingliz tili" },
   { login: "marjona", password: "8857640101", subject: "Rus tili" },
   { login: "admin", password: "admin123", subject: " Admin " },
+  { login: "admin", password: "admin123", subject: " Admin " },
+
   // Yana qo‘shmoqchi bo‘lsang shu yerga yoz
 ];
 
